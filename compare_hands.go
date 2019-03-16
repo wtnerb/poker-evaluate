fc455fee57f02e244a51381ed6c15185d8314515
wtnerb <wtnerb@gmail.com>
2019-03-15 18:43:59 -0700

    seven card compare by hand rank works

--- compare_hands.go
+++ compare_hands.go
@@ -3,3 +3,21 @@ package main
 func holdemCompare(tableCards []card, holes [][]card) (int, bestHand) {
 	return 0, bestHand{}
 }
+
+const (
+	LEFT = iota
+	RIGHT
+	TIE
+)
+
+func sevenCardCompare(left, right []card) int {
+	l := buildBestHand(left)
+	r := buildBestHand(right)
+	if l.rank > r.rank {
+		return LEFT
+	}
+	if r.rank > l.rank {
+		return RIGHT
+	}
+	return TIE
+}
--- holdemCompare_test.go
+++ holdemCompare_test.go
@@ -2,48 +2,107 @@ package main
 
 import "testing"
 
-func TestHoldemCompare(t *testing.T) {
-	test := struct {
-		tableCards []card
-		holes      [][]card
-		expected   struct {
-			winner int
-			hand   bestHand
-		}
+// func TestHoldemCompare(t *testing.T) {
+// 	test := struct {
+// 		tableCards []card
+// 		holes      [][]card
+// 		expected   struct {
+// 			winner int
+// 			hand   bestHand
+// 		}
+// 	}{
+// 		[]card{
+// 			card{ACE, SPADE},
+// 			card{KING, DIAMOND},
+// 			card{TWO, HEART},
+// 			card{SEVEN, HEART},
+// 			card{TEN, CLUB},
+// 		},
+// 		[][]card{
+// 			[]card{card{FOUR, SPADE}, card{QUEEN, CLUB}},
+// 			[]card{card{THREE, SPADE}, card{KING, SPADE}},
+// 		},
+// 		struct {
+// 			winner int
+// 			hand   bestHand
+// 		}{
+// 			1,
+// 			bestHand{
+// 				[5]card{
+// 					card{ACE, SPADE},
+// 					card{KING, DIAMOND},
+// 					card{KING, SPADE},
+// 					card{TEN, CLUB},
+// 					card{SEVEN, HEART},
+// 				},
+// 				pair,
+// 			},
+// 		},
+// 	}
+// 	winner, best := holdemCompare(test.tableCards, test.holes)
+// 	if winner != test.expected.winner {
+// 		t.Error("the wrong person won")
+// 	}
+// 	if len(best.cards) != 5 || len(test.expected.hand.cards) != 5 {
+// 		t.Error("wrong number of cards")
+// 	}
+// }
+
+func TestSevenCardCompare(t *testing.T) {
+	tests := []struct {
+		left   []card
+		right  []card
+		winner int
 	}{
-		[]card{
-			card{ACE, SPADE},
-			card{KING, DIAMOND},
-			card{TWO, HEART},
-			card{SEVEN, HEART},
-			card{TEN, CLUB},
-		},
-		[][]card{
-			[]card{card{FOUR, SPADE}, card{QUEEN, CLUB}},
-			[]card{card{THREE, SPADE}, card{KING, SPADE}},
-		},
-		struct {
-			winner int
-			hand   bestHand
-		}{
-			1,
-			bestHand{
-				[5]card{
-					card{ACE, SPADE},
-					card{KING, DIAMOND},
-					card{KING, SPADE},
-					card{TEN, CLUB},
-					card{SEVEN, HEART},
-				},
-				pair,
+		{
+			[]card{
+				card{ACE, SPADE},
+				card{KING, DIAMOND},
+				card{TWO, HEART},
+				card{SEVEN, HEART},
+				card{TEN, CLUB},
+				card{FOUR, SPADE},
+				card{QUEEN, CLUB},
 			},
+			[]card{
+				card{ACE, SPADE},
+				card{KING, DIAMOND},
+				card{TWO, HEART},
+				card{SEVEN, HEART},
+				card{TEN, CLUB},
+				card{EIGHT, DIAMOND},
+				card{KING, CLUB},
+			},
+			RIGHT,
+		},
+		{
+			[]card{
+				card{ACE, SPADE},
+				card{SEVEN, DIAMOND},
+				card{TWO, HEART},
+				card{SEVEN, HEART},
+				card{TEN, CLUB},
+				card{FOUR, SPADE},
+				card{ACE, CLUB},
+			},
+			[]card{
+				card{ACE, SPADE},
+				card{KING, DIAMOND},
+				card{TWO, HEART},
+				card{SEVEN, HEART},
+				card{TEN, CLUB},
+				card{EIGHT, DIAMOND},
+				card{KING, CLUB},
+			},
+			LEFT,
 		},
 	}
-	winner, best := holdemCompare(test.tableCards, test.holes)
-	if winner != test.expected.winner {
-		t.Error("the wrong person won")
-	}
-	if len(best.cards) != 5 || len(test.expected.hand.cards) != 5 {
-		t.Error("wrong number of cards")
+
+	for _, test := range tests {
+		result := sevenCardCompare(test.left, test.right)
+
+		if result != test.winner {
+			t.Error("compare failed! recieved index", result, "\nfrom input", test)
+		}
 	}
 }
